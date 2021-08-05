359a2b6d9d0b6cbd79e448bf4e7ff77495132e1b
KoopaCool <koopacool135@gmail.com>
2021-08-06 01:26:00 +0200

    Update registers

--- src/main/java/xyz/rinsenpai/hololivegachahell/core/init/BlockInit.java
+++ src/main/java/xyz/rinsenpai/hololivegachahell/core/init/BlockInit.java
@@ -14,9 +14,9 @@ public class BlockInit {
 	public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, HololiveGachaHell.MOD_ID);
 	
 	// REGISTERING BLOCKS
-	public static final net.minecraftforge.fmllegacy.RegistryObject<TitaniumOre> TITANIUM_ORE = BLOCKS.register("titanium_ore", () -> new TitaniumOre());
-	public static final net.minecraftforge.fmllegacy.RegistryObject<MetalBlockBase> TITANIUM_BLOCK = BLOCKS.register("titanium_block", () -> new MetalBlockBase());
-	public static final net.minecraftforge.fmllegacy.RegistryObject<MysticShardOre> MYSTIC_SHARD_ORE = BLOCKS.register("mystic_shard_ore", () -> new MysticShardOre());
-	public static final net.minecraftforge.fmllegacy.RegistryObject<GachaBlock> GACHA_BLOCK = BLOCKS.register("gacha_block", () -> new GachaBlock());
+	public static final net.minecraftforge.fmllegacy.RegistryObject<Block> TITANIUM_ORE = BLOCKS.register("titanium_ore", () -> new TitaniumOre());
+	public static final net.minecraftforge.fmllegacy.RegistryObject<Block> TITANIUM_BLOCK = BLOCKS.register("titanium_block", () -> new MetalBlockBase());
+	public static final net.minecraftforge.fmllegacy.RegistryObject<Block> MYSTIC_SHARD_ORE = BLOCKS.register("mystic_shard_ore", () -> new MysticShardOre());
+	public static final net.minecraftforge.fmllegacy.RegistryObject<Block> GACHA_BLOCK = BLOCKS.register("gacha_block", () -> new GachaBlock());
 
 }
