e616c0a01e6ae677a80b386f687399314eacc8a6
KoopaCool <koopacool135@gmail.com>
2021-08-15 22:10:15 +0200

    Balance ore generation (again)

--- src/main/java/xyz/rinsenpai/hololivegachahell/world/OreGeneration.java
+++ src/main/java/xyz/rinsenpai/hololivegachahell/world/OreGeneration.java
@@ -16,8 +16,8 @@ public class OreGeneration {
 
 	public static void generateOres(final BiomeLoadingEvent event) {
 		if(!((event.getCategory().equals(Biome.BiomeCategory.THEEND)) || (event.getCategory().equals(Biome.BiomeCategory.NETHER)))) {
-			generateOre(event.getGeneration(), OreConfiguration.Predicates.NATURAL_STONE, BlockInit.MYSTIC_SHARD_ORE.get().defaultBlockState(), 3, 5, 16, 8);
-			generateOre(event.getGeneration(), OreConfiguration.Predicates.NATURAL_STONE, BlockInit.TITANIUM_ORE.get().defaultBlockState(), 6, 5, 24, 12);
+			generateOre(event.getGeneration(), OreConfiguration.Predicates.NATURAL_STONE, BlockInit.MYSTIC_SHARD_ORE.get().defaultBlockState(), 4, 5, 16, 4);
+			generateOre(event.getGeneration(), OreConfiguration.Predicates.NATURAL_STONE, BlockInit.TITANIUM_ORE.get().defaultBlockState(), 8, 5, 24, 8);
 		}
 		
 		if(event.getCategory().equals(Biome.BiomeCategory.NETHER)) {
