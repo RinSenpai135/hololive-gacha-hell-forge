aa2e2477d234cb4971fa398f18bce5deb23dbfe6
KoopaCool <koopacool135@gmail.com>
2021-08-25 22:01:27 +0200

    Update TokenItem.java

--- src/main/java/xyz/rinsenpai/hololivegachahell/common/items/TokenItem.java
+++ src/main/java/xyz/rinsenpai/hololivegachahell/common/items/TokenItem.java
@@ -7,6 +7,6 @@ public class TokenItem extends Item {
 
 	public TokenItem() {
 		super(new Item.Properties()
-				.tab(HololiveGachaHell.MAIN_MOD_TAB));
+				.tab(HololiveGachaHell.TOKEN_TAB));
 	}
 }
